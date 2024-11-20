101d539947714f02293a29a936036555eccf7e82
gcanat <gui.canat@gmail.com>
2024-11-20 10:42:33 +0100

    add tokenizer for shakespeare data

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,11 @@ use data::Dataset;
 
 fn main() -> anyhow::Result<()> {
     let train_cfg = TrainConfig::default();
-    let mut ds = Dataset::new("data/input.txt.gz", None, train_cfg.seq_len());
+    let mut ds = Dataset::new(
+        "data/input.txt.gz",
+        Some("data/shakespeare_tokenizer.json"),
+        train_cfg.seq_len(),
+    );
     training_loop(&mut ds, &train_cfg)?;
     Ok(())
 }
